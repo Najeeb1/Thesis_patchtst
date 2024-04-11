06dcccde47ee7dc5eebe679887ea3d32c2937f39
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2024-04-11 15:47:30 +0200

    changing main for mmnist

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def create_parser():
     parser.add_argument('--batch_size', default=4, type=int, help='Batch size')
     parser.add_argument('--val_batch_size', default=4, type=int, help='Batch size')
     parser.add_argument('--data_root', default='./data/')
-    parser.add_argument('--dataname', default='taxibj', choices=['mmnist', 'taxibj', 'kth', 'kitti_caltech'])
+    parser.add_argument('--dataname', default='mmnist', choices=['mmnist', 'taxibj', 'kth', 'kitti_caltech'])
     parser.add_argument('--num_workers', default=8, type=int)
 
     # model parameters
-    parser.add_argument('--in_shape', default= [4, 2, 32, 32], type=int,nargs='*') # [10, 1, 64, 64] for mmnist, [4, 2, 32, 32] for taxibj  
+    parser.add_argument('--in_shape', default= [10, 1, 64, 64], type=int,nargs='*') # [10, 1, 64, 64] for mmnist, [4, 2, 32, 32] for taxibj  
     
                                                                                   # [10, 1, 128, 128] for kth, [10, 3, 128, 160] for caltech 
     parser.add_argument('--hid_S', default=64, type=int)
@@ -32,7 +32,7 @@ def create_parser():
     parser.add_argument('--groups', default=4, type=int)
 
     # Training parameters
-    parser.add_argument('--epochs', default=2, type=int)
+    parser.add_argument('--epochs', default=150, type=int)
     parser.add_argument('--log_step', default=1, type=int)
     parser.add_argument('--lr', default=0.01, type=float, help='Learning rate')
     return parser
