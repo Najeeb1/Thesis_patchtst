cd93838ef1b5411fc306a0589b6310721bace59c
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2024-04-11 15:58:00 +0200

    changing dataloader for moving_mnist again

--- API/dataloader.py
+++ API/dataloader.py
@@ -5,7 +5,7 @@ from .dataloader_moving_mnist import load_data as load_mmnist
 
 def load_data(dataname,batch_size, val_batch_size, data_root, num_workers, **kwargs):
     if dataname == 'mmnist':
-        return load_taxibj(batch_size, val_batch_size, data_root, num_workers)
+        return load_mmnist(batch_size, val_batch_size, data_root, num_workers)
     # elif dataname == 'taxibj':
     #     return load_mmnist(batch_size, val_batch_size, data_root, num_workers)
     # elif dataname == 'mmnist':
